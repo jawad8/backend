623fc55dfb0b6d839a445f20b4b02c39b11ffb83
Jawad Shaikh <jawad.shaikh@cyphware.com>
2022-07-16 13:11:18 +0530

    16072022 midday

--- logs/models.py
+++ logs/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 # Create your models here.
 
 
-class LogTable():
+class LogTable(models.Model):
     log_type = models.CharField(max_length=100)
     log_time = models.DateTimeField(auto_now_add = True)
     log_error = models.CharField(max_length=100)

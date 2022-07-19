c3d42f3ec58b9f394f5c7ff0712c77c86fceb108
Jawad Shaikh <jawad.shaikh@cyphware.com>
2022-07-19 13:54:43 +0530

    154 19072022

--- logs/views.py
+++ logs/views.py
@@ -6,6 +6,10 @@ from . import models, serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import logging
+from datetime import date
+from datetime import timedelta
+
+
 logger = logging.getLogger('db',)
 
 # api for creating logs
@@ -32,13 +36,14 @@ def fetchLogs(request):
         return Response(False, status=400)
 
 
-
 # filter api for logs
 @api_view(['POST'])
 def filterLogs(request):
     filteredList = []
     reqData = request.data
-    print(reqData)
+    today = date.today()
+    yesterday = today - timedelta(days = 1)
+    # checks multiple filter types and create a filtered list to send response as requested
     if 'info' in reqData:
         res_obj = models.LogTable.objects.filter(log_type=reqData['info'])
         serializer_obj = serializers.GetLogsSerializer(res_obj, many=True)
@@ -47,27 +52,34 @@ def filterLogs(request):
     if 'error' in reqData:
         res_obj = models.LogTable.objects.filter(log_type=reqData['error'])
         serializer_obj = serializers.GetLogsSerializer(res_obj, many=True)
-        print(serializer_obj)
         filteredList.extend(serializer_obj.data)
     if 'debug' in reqData:
         res_obj = models.LogTable.objects.filter(log_type=reqData['debug'])
         serializer_obj = serializers.GetLogsSerializer(res_obj, many=True)
-        print(serializer_obj)
         filteredList.extend(serializer_obj.data)
     if 'critical' in reqData:
         res_obj = models.LogTable.objects.filter(log_type=reqData['critical'])
         serializer_obj = serializers.GetLogsSerializer(res_obj, many=True)
-        print(serializer_obj)
         filteredList.extend(serializer_obj.data)
     if 'warning' in reqData:
         res_obj = models.LogTable.objects.filter(log_type=reqData['warning'])
         serializer_obj = serializers.GetLogsSerializer(res_obj, many=True)
-        print(serializer_obj)
+        filteredList.extend(serializer_obj.data)
+    today = date.today()
+    yesterday = today - timedelta(days = 1)
+    # from 12 - 6pm
+    if 'today' in reqData:
+        res_obj = models.LogTable.objects.filter(log_time__gte= yesterday )
+        serializer_obj = serializers.GetLogsSerializer(res_obj, many=True)
+        filteredList.extend(serializer_obj.data)
+    # from 6 - 12am
+    if 'yesterday' in reqData:
+        res_obj = models.LogTable.objects.filter(log_time__gte=(yesterday - timedelta(days = 1)),log_time__lte = yesterday)
+        serializer_obj = serializers.GetLogsSerializer(res_obj, many=True)
         filteredList.extend(serializer_obj.data)
     if not reqData:
         res_obj = models.LogTable.objects.all()
         serializer_obj = serializers.GetLogsSerializer(res_obj, many=True)
-
         return Response(serializer_obj.data)
-
     return Response(filteredList)
+
